718967b429f39a208401f49d95ceb23830f75860
R Sloan <rnsloan1@gmail.com>
2025-11-01 17:51:58 +1100

    Initial Commit

---
+++ PureClip/PureClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureClipApp.swift
+//  PureClip
+//
+//  Created by Rowan on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PureClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
